5a8fe26d123da4bc2d5c4af687ac1fd2db7bf7cf
Rodrigo de Sousa <rodrigo.ross.comp@gmail.com>
2022-08-28 01:18:46 -0400

    test: :white_check_mark: altera regra de aceite de cadastro de usuario

--- cypress/integrations/cadastro.feature
+++ cypress/integrations/cadastro.feature
@@ -5,20 +5,20 @@ Funcionalidade: Cadastro de novos usuarios
     Como usuário não autenticado
     Eu quero me cadastrar na plataforma
 
-Cenário: Usuário não autenticado acessa tela de cadastro
-    Dado usuário na tela de Home
-    E usuário não está cadastrado
-    Quando clicar no botão de cadastre-se
-    Então deve ser redirecionado para tela de Cadastro
-
-Cenário: Novo usuário cadastrado é redirecionado para dashboard
+Cenário: Novo usuário cadastrado recebe confirmação de cadastro
     Dado usuário na tela de Cadastro
     E com todas as informações de cadastro preenchidas
     Quando clicar no botão de cadastrar
-    Então usuário deve ser redirecionado para tela de dashboard
+    Então usuário deve visualizar mensagem de cadastro bem sucedido
 
 Cenário: Usúario que não fornecer um e-mail deve visualizar uma mensagem de erro
     Dado usuário na tela de Cadastro
     E não forneceu somente dados de email
     Quando clicar no botão de cadastrar
     Então deve visualizar mensagem de erro informando que o e-mail é obrigatório
+
+Cenário: Usuário que informar valores de senha divergentes deve visualizar erro
+    Dado usuário na tela de Cadastro
+    E forneceu confirmação de senha divergente
+    Quando clicar no botão de cadastrar
+    Então deve visualizar mensagem de erro informando que senhas não conferem
--- cypress/integrations/cadastro.js
+++ cypress/integrations/cadastro.js
@@ -1,30 +1,13 @@
-describe("Usuário não autenticado acessa tela de cadastro", () => {
-    Given('usuário na tela de Home', () => {
-        cy.visit("http://localhost:9088/");
-    });
-
-    And("usuário não está cadastrado", () => { })
-
-    When("clicar no botão de cadastre-se", () => {
-        cy.get("a[id='cadastrar']")
-            .should('have.attr', 'href', 'http://localhost:8088/cadastrar')
-            .click();
-    })
-
-    Then("deve ser redirecionado para tela de Cadastro", () => {
-        cy.url()
-            .should('be.equal', 'http://localhost:9088/cadastrar');
-    })
-})
-
-describe("Novo usuário cadastrado é redirecionado para dashboard", () => {
+describe("Novo usuário cadastrado recebe confirmação de cadastro", () => {
     Given('usuário na tela de Cadastro', () => {
-        cy.visit("http://localhost:9088/cadastrar");
+        cy.visit("/cadastrar");
     });
 
     And("com todas as informações de cadastro preenchidas", () => {
+        console.log("entrei");
         cy.get("input[id='email']").type('teste@teste.com');
         cy.get("input[id='senha']").type('123Mudar');
+        cy.get("input[id='confirma-senha']").type('123Mudar');
     })
 
     When("clicar no botão de cadastrar", () => {
@@ -32,19 +15,19 @@ describe("Novo usuário cadastrado é redirecionado para dashboard", () => {
             .click();
     })
 
-    Then("usuário deve ser redirecionado para tela de dashboard", () => {
-        cy.url()
-            .should('be.equal', "http://localhost:9088/dashboard");
+    Then("usuário deve visualizar mensagem de cadastro bem sucedido", () => {
+        cy.get('#retorno').contains("Cadastro realizado com sucesso")
     })
 })
 
 describe("Usuário que não fornecer um e-mail deve visualizar uma mensagem de erro", () => {
     Given('usuário na tela de Cadastro', () => {
-        cy.visit("http://localhost:9080/cadastrar");
+        cy.visit("/cadastrar");
     });
 
-    And("com todas as informações de cadastro preenchidas", () => {
+    And("não forneceu somente dados de email", () => {
         cy.get("input[id='senha']").type('123Mudar');
+        cy.get("input[id='confirma-senha']").type('123Mudar');
     })
 
     When("clicar no botão de cadastrar", () => {
@@ -53,6 +36,28 @@ describe("Usuário que não fornecer um e-mail deve visualizar uma mensagem de e
     })
 
     Then("deve visualizar mensagem de erro informando que o e-mail é obrigatório", () => {
-        cy.contains("E-mail é obrigatório");
+        cy.get('#retorno').contains("E-mail é obrigatório");
+    })
+})
+
+describe("Usuário que informar valores de senha divergentes deve visualizar erro", () => {
+    Given('usuário na tela de Cadastro', () => {
+        cy.visit("/cadastrar");
+    });
+
+    And("forneceu confirmação de senha divergente", () => {
+        console.log("entrei");
+        cy.get("input[id='email']").type('teste@teste.com');
+        cy.get("input[id='senha']").type('123Mudar');
+        cy.get("input[id='confirma-senha']").type('123Diferente');
+    })
+
+    When("clicar no botão de cadastrar", () => {
+        cy.get("button[id='btn-cadastro']")
+            .click();
+    })
+
+    Then("deve visualizar mensagem de erro informando que senhas não conferem", () => {
+        cy.get('#retorno').contains("Senhas informadas são divergentes")
     })
 })
